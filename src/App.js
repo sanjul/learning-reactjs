df9d2256ddb43ed8a15e42f0f27f9f45ca8ce99a
Sanjul <sanjul@users.noreply.github.com>
2020-05-06 00:17:57 +0530

    SLICE to clone an array and splice to mutate, splice can remove as well as replace

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ class App extends Component {
   }
 
   deletePerson = (personIndex) => {
-    const persons = this.state.persons;
+    const persons = this.state.persons.slice();
     persons.splice(personIndex, 1);
     this.setState({ persons: persons });
   }
