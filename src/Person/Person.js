d3b19d81a92410c8b568ac9de1274eb1125d3c69
Sanjul <sanjul@users.noreply.github.com>
2020-05-05 21:45:33 +0530

    add css for person

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,14 +1,10 @@
 import React from 'react';
-
+import './Person.css';
 
 const person = (props) => {
 
-    const childStyle = {
-        backgroundColor : 'grey'
-    }
-
     return (
-        <div onClick={props.onClick}>
+        <div onClick={props.onClick} class="Person">
             <p>Hello from {Math.floor(Math.random() * 30)} year old {props.name}!</p>
             <input onChange={props.changed} type="text" value={props.name}/>
         </div>
