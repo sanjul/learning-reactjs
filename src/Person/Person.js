da24e9131f1ae481eaf9720b942f743b5d0f6533
Sanjul <sanjul@users.noreply.github.com>
2020-05-10 20:33:06 +0530

    some style cleanup

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -2,23 +2,12 @@ import React from 'react';
 import classes from './Person.module.css';
 
 
-const person = (props) => {
-
-    const delStyle = {
-        color:'red',
-        fontSize : '10px',
-        cursor: 'pointer'
-    };
-
-
-    return (
-        <div className={classes.Person}>
-            <span style={delStyle} onClick={props.onDelete}>Delete</span>
-            <p>Hello from {props.age} year old <b>{props.name}!</b></p>
-            <input onChange={props.changed} type="text" value={props.name}/>
-        </div>
-    )
-
-};
+const person = (props) => (
+    <div className={classes.Person}>
+        <button className={classes.DeleteButton} onClick={props.onDelete}>Delete</button>
+        <p>Hello from {props.age} year old <b>{props.name}!</b></p>
+        <input onChange={props.changed} type="text" value={props.name} />
+    </div>
+);
 
 export default person;
