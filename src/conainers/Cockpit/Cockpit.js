9a394a1c01ce13015e6cacbf0fedb2e992cc43fd
Sanjul <sanjul@users.noreply.github.com>
2020-05-10 22:28:08 +0530

    added keys in cockpit list to get rid of warning

--- src/conainers/Cockpit/Cockpit.js
+++ src/conainers/Cockpit/Cockpit.js
@@ -6,16 +6,16 @@ import Logo from '../../components/Logo/Logo';
 
 const cockpit = props => [
 
-    <header className={classes.Header}>
+    <header className={classes.Header} key="cockpit-header">
         <h1>My react app</h1>
         <Logo />
     </header>,
 
-    <div className={classes.Body}>
+    <div className={classes.Body} key="cockpit-children">
         {props.children}
     </div>,
-    
-    <footer className={classes.Footer}>
+
+    <footer className={classes.Footer} key="cockpit-footer">
         <span>(c) {new Date().getFullYear()}</span>
     </footer>
 
