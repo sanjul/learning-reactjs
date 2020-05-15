fe761905253f0ac8b1750afaf8be0e15122f0052
Sanjul <sanjul@users.noreply.github.com>
2020-05-15 23:30:06 +0530

    Using a Higher Order Component Aux to return multiple JSX Componnents

--- src/conainers/Cockpit/Cockpit.js
+++ src/conainers/Cockpit/Cockpit.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import classes from './Cockpit.module.css';
 import Logo from '../../components/Logo/Logo';
+import Aux from '../../hoc/Aux';
 
 
+const cockpit = props => (
 
-const cockpit = props => [
+    <Aux>
+        <header className={classes.Header} key="cockpit-header">
+            <h1>My react app</h1>
+            <Logo />
+        </header>
 
-    <header className={classes.Header} key="cockpit-header">
-        <h1>My react app</h1>
-        <Logo />
-    </header>,
+        <div className={classes.Body} key="cockpit-children">
+            {props.children}
+        </div>
 
-    <div className={classes.Body} key="cockpit-children">
-        {props.children}
-    </div>,
+        <footer className={classes.Footer} key="cockpit-footer">
+            <span>(c) {new Date().getFullYear()}</span>
+        </footer>
+    </Aux>
 
-    <footer className={classes.Footer} key="cockpit-footer">
-        <span>(c) {new Date().getFullYear()}</span>
-    </footer>
-
-];
+);
 
 export default cockpit;
